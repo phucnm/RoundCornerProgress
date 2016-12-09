2818ef80694ba453cbdb1e5c675db7f9bd035301
Phuc Nguyen <phucnguyenpr@gmail.com>
2016-12-09 11:46:02 +0700

    Add progress label

--- RoundCornerProgressView/RoundCornerProgressView.swift
+++ RoundCornerProgressView/RoundCornerProgressView.swift
@@ -10,6 +10,20 @@ import UIKit
 @IBDesignable
 public class RoundCornerProgressView: UIView {
     
+    public var progressLabel = UILabel(frame: .zero)
+    
+    @IBInspectable public var labelDisplay: Bool = true {
+        didSet {
+            progressLabel.isHidden = !labelDisplay
+        }
+    }
+    
+    @IBInspectable public var labelColor: UIColor = UIColor.black {
+        didSet {
+            progressLabel.textColor = labelColor
+        }
+    }
+    
     @IBInspectable public var trackRoundCorners: UIRectCorner = [.topRight, .bottomRight, .topLeft, .bottomLeft] {
         didSet {
             trackView.roundCorner(roundingCorners: trackRoundCorners, cornerRadius: CGSize(width: frame.size.height / 2, height: frame.size.height / 2))
@@ -25,7 +39,7 @@ public class RoundCornerProgressView: UIView {
     
     private var trackView = UIView(frame: .zero)
     
-    @IBInspectable public var trackTintColor = UIColor.lightGray {
+    @IBInspectable public var trackTintColor: UIColor = UIColor.lightGray {
         didSet {
             trackView.backgroundColor = trackTintColor
         }
@@ -33,7 +47,7 @@ public class RoundCornerProgressView: UIView {
     
     private var progressView = UIView(frame: .zero)
     
-    @IBInspectable public var progressTintColor = UIColor.red {
+    @IBInspectable public var progressTintColor: UIColor = UIColor.red {
         didSet {
             progressView.backgroundColor = progressTintColor
         }
@@ -48,6 +62,7 @@ public class RoundCornerProgressView: UIView {
             }
             progressView.frame = CGRect(origin: progressView.frame.origin, size: CGSize(width: frame.width * progress, height: frame.height))
             progressView.roundCorner(roundingCorners: progressRoundCorners, cornerRadius: CGSize(width: frame.size.height / 2, height: frame.size.height / 2))
+            progressLabel.text = String(format: "%d%%", Int(progress * 100))
         }
     }
     
@@ -77,6 +92,15 @@ public class RoundCornerProgressView: UIView {
         progressView.roundCorner(roundingCorners: progressRoundCorners, cornerRadius: CGSize(width: frame.size.height / 2, height: frame.size.height / 2))
         progressView.backgroundColor = progressTintColor
         trackView.addSubview(progressView)
+        
+        progressLabel = UILabel(frame: CGRect(origin: .zero, size: CGSize(width: frame.width, height: frame.height)))
+        progressLabel.textAlignment = .center
+        bringSubview(toFront: progressLabel)
+        progressLabel.center = CGPoint(x: frame.width / CGFloat(2), y: frame.height / CGFloat(2))
+        progressLabel.text = String(format: "%d%%", Int(progress * 100))
+        progressLabel.textColor = labelColor
+        progressLabel.isHidden = !labelDisplay
+        addSubview(progressLabel)
     }
 }
 
