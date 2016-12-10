ad63983d89f9e6b748c930f1e0f574466d3f9509
Chi-Quyen Le <quyen.lechi@gmail.com>
2016-12-10 18:59:45 +0700

    add example

---
+++ Example/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Chi-Quyen Le on 12/10/16.
+//  Copyright © 2016 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+import RoundCornerProgressView
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var progressView: RoundCornerProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RoundCornerProgressView/UIView+RoundCorner.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+RoundCorner.swift
+//  RoundCornerProgressView
+//
+//  Created by Chi-Quyen Le on 12/10/16.
+//  Copyright © 2016 Phuc Nguyen. All rights reserved.
+//
+
+import Foundation
